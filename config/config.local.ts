5b6364cab82e36e0130acabe473ca1c8b9b5118c
ljw1412 <506644791@qq.com>
2020-09-14 20:13:45 +0800

    chore: 更新

--- config/config.local.ts
+++ config/config.local.ts
@@ -2,7 +2,9 @@ import { EggAppConfig, PowerPartial } from 'egg';
 
 export default () => {
   const config: PowerPartial<EggAppConfig> = {
-    security: { domainWhiteList: ['http://localhost:8080'] },
+    security: {
+      domainWhiteList: ['http://localhost:8080', 'http://localhost:8081']
+    },
     cors: {
       // origin: ctx => ctx.header.origin,
       // 是否带cookie
