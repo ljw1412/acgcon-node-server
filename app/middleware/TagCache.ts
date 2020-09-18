da563fa30babeadc008f950016e70db670370773
ljw1412 <506644791@qq.com>
2020-09-19 00:12:31 +0800

    chore: 修复重置缓存

--- app/middleware/TagCache.ts
+++ app/middleware/TagCache.ts
@@ -5,9 +5,9 @@ import { TAG_PREFIX } from '../constant/cache';
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
 export default _options => async (ctx: Context, next: Next) => {
   const { app } = ctx;
-  const { acgType, type } = ctx.request.body;
-  if (acgType && type) {
-    const cacheKey = `${TAG_PREFIX}${acgType}_${type}`;
+  const { acgType, subType } = ctx.request.body;
+  if (acgType && subType) {
+    const cacheKey = `${TAG_PREFIX}${acgType}_${subType}`;
     await app.redis.del(cacheKey);
   }
   await next();
